0c89b7c4ca0c2e3c7418c113366c07f1801761ca
Sujeet Kumar Gupta <sujeetgupta2801@gmail.com>
2025-05-01 17:39:28 +0530

    feat(client): Add Protected route wrapper and reusable Input component with forwardRef

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Select from "./Select";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./Container/Container";
+import Logo from "./Logo";
+import LogoutButton from "./Header/LogoutButton";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import Button from "./Button";
+import PostForm from "./PostForm/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from './Input'
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutButton,
+    RTE,
+    Select,
+    Button,
+    Input,
+    PostForm,
+    Signup,
+    Login,
+    PostCard,
+    AuthLayout
+}
