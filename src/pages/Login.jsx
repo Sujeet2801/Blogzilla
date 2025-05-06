6f91910b5a44f2ee9d30500f8f3b8fa1fe8f74f4
Sujeet Kumar Gupta <sujeetgupta2801@gmail.com>
2025-05-06 09:11:33 +0530

    feat(client/pages): Add Login page with reusable LoginComponent

---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Login as LoginComponent } from '../components/index' 
+function Login() {
+  return (
+    <div className='py-8'>
+      <h1>welcome 1</h1>
+        <LoginComponent />
+    </div>
+  )
+}
+
+export default Login
