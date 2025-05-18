6f28f95af7d18f614c6a5fecddea9e4f51e08259
Sujeet Kumar Gupta <sujeetgupta2801@gmail.com>
2025-05-18 08:31:56 +0530

    refactor(client/components): Update post card

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,20 +1,25 @@
+// PostCard.jsx
 import React from 'react'
 import appwriteService from '../appwrite/config'
 import { Link } from 'react-router-dom'
 
 function PostCard({ $id, title, featuredImage }) {
-
     return (
-        <Link to={`/post/${$id}`}>
-            <div className="w-full bg-gray-100 rounded-xl p-4">
-                <div className="w-full justify-center mb-4">
-                    <img src={appwriteService.getPreview(featuredImage)} alt="title"
-                        className=' rounded-xl' />
+        <Link to={`/post/${$id}`} className="block h-full">
+            <div className="flex flex-col bg-white rounded-2xl shadow-md hover:shadow-lg
+            transition duration-300 h-full overflow-hidden">
+                <div className="w-full h-56 overflow-hidden">
+                    <img
+                        src={appwriteService.getPreview(featuredImage)}
+                        alt={title}
+                        className="w-full h-full object-cover"
+                    />
                 </div>
-
-                <h2 className=' text-xl font-bold'>
-                    {title}
-                </h2>
+                <div className="p-4 flex-grow flex items-center">
+                    <h2 className="text-lg font-semibold text-gray-800 text-center">
+                        {title}
+                    </h2>
+                </div>
             </div>
         </Link>
     )
