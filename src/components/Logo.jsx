8f907dbc090fb1f1ddccb3cda848aaee8621a641
Sujeet Kumar Gupta <sujeetgupta2801@gmail.com>
2025-05-18 08:20:46 +0530

    refactor(client/components): Update customizable Logo component with adjustable width and className props

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-function Logo({ width = '100px' }) {
+function Logo({ width = 'auto', className = '' }) {
   return (
     <div
-      className="text-3xl font-extrabold text-primary tracking-wide"
+      className={`text-3xl ml-10 md:text-4xl font-extrabold tracking-wide text-black ${className}`}
       style={{ width }}
+      aria-label="Blogzilla Logo"
     >
       Blogzilla
     </div>
