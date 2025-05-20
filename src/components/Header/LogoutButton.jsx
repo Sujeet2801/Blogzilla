4cb8fc8e5fb0d7cd2597d99f6436bb74e15e51cd
Sujeet Kumar Gupta <sujeetgupta2801@gmail.com>
2025-05-20 22:43:23 +0530

    refactor(client/components/Header): Change style of button

--- src/components/Header/LogoutButton.jsx
+++ src/components/Header/LogoutButton.jsx
@@ -15,8 +15,8 @@ function LogoutButton() {
   return (
 
     <button
-      className=' inline-block px-6 py-2 duration-200 
-    hover:bg-blue-100 rounded-full'
+      className=' inline-block px-6 py-2 duration-200 border text-white 
+      hover:bg-blue-100 hover:text-blue-700 rounded-full'
       onClick={logoutHandler}
     >Logout</button>
   )
