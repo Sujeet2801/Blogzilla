3c98c6b25153d84365cecf2139e5c06041e5e9a1
Sujeet Kumar Gupta <sujeetgupta2801@gmail.com>
2025-05-21 07:25:25 +0530

    feat(client/pages): Login component added in login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,6 @@ import { Login as LoginComponent } from '../components/index'
 function Login() {
   return (
     <div className='py-8'>
-      <h1>welcome 1</h1>
         <LoginComponent />
     </div>
   )
